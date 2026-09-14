e6637f0f82e40458ad4933982e8add3f324db8cb
sgurungp <53279830+sgurungp@users.noreply.github.com>
2026-09-13 21:33:02 -0400

    Update files and scripts to reflect move from A1A2 naming to italian-201.

--- shell/gen-epub.sh
+++ shell/gen-epub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script to generate EPUB file from Markdown
 #
-pandoc --number-sections --metadata-file=yaml/ita-A1A2.yaml md/ItalianA1A2.md -o epub/ItalianA1A2.epub
+pandoc --number-sections --metadata-file=yaml/italian-201.yaml md/italian-201.md -o epub/italian-201.epub
